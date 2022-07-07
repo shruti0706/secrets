1a738e7bf8e6005de648774b6e216458d0dafedd
shruti0706 <72216138+shruti0706@users.noreply.github.com>
2022-07-07 18:06:01 +0530

    level 1 authentication

---
+++ app.js
@@ -0,0 +1,65 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const ejs=require("ejs");
+const mongoose=require("mongoose");
+
+const app=express();
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended : true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema={
+  email:String,
+  password:String
+};
+
+
+const User=new mongoose.model("user",userSchema);
+
+app.get("/",function(req,res){
+  res.render("home.ejs");
+});
+
+app.get("/login",function(req,res){
+  res.render("login.ejs");
+});
+
+app.get("/register",function(req,res){
+  res.render("register.ejs");
+});
+
+app.post("/register",function(req, res){
+const newUser= new User({
+email: req.body.username,
+password: req.body.password
+});
+newUser.save(function(err){
+  if(err)
+  console.log(err);
+  else{
+    res.render("secrets.ejs");
+  }
+});
+});
+
+app.post("/login",function(req, res){
+  const username= req.body.username;
+  const password= req.body.password;
+
+  User.findOne({email:username},function(err,foundUser){
+    if(err)
+    console.log(err);
+    else{
+      if(foundUser)
+        if(foundUser.password === password)
+        res.render("secrets.ejs");
+    }
+  });
+
+});
+
+app.listen("3000",function(){
+  console.log("server is up and running on port 3000");
+});
