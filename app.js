fb578e689211cf489ad80c03a69beca5d75ff4da
shruti0706 <72216138+shruti0706@users.noreply.github.com>
2022-07-07 18:07:24 +0530

    level 2 authentication

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
+const encrypt=require("mongoose-encryption");
 
 const app=express();
 app.use(express.static("public"));
@@ -10,11 +11,13 @@ app.use(bodyParser.urlencoded({extended : true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
-const userSchema={
+const userSchema=new mongoose.Schema({
   email:String,
   password:String
-};
+});
 
+const secret="Thisisourlittlesecret";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User=new mongoose.model("user",userSchema);
 
