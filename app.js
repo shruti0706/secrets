544c187358135e584dff77209c14260115ded508
shruti0706 <72216138+shruti0706@users.noreply.github.com>
2022-07-07 22:14:17 +0530

    level 4- using md5 hashing

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+const md5 = require('md5');
 
 const app=express();
+
 app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended : true}));
@@ -16,8 +17,6 @@ const userSchema=new mongoose.Schema({
   email:String,
   password:String
 });
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User=new mongoose.model("user",userSchema);
 
@@ -36,7 +35,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req, res){
 const newUser= new User({
 email: req.body.username,
-password: req.body.password
+password: md5(req.body.password)
 });
 newUser.save(function(err){
   if(err)
@@ -49,7 +48,7 @@ newUser.save(function(err){
 
 app.post("/login",function(req, res){
   const username= req.body.username;
-  const password= req.body.password;
+  const password= md5(req.body.password) ;
 
   User.findOne({email:username},function(err,foundUser){
     if(err)
