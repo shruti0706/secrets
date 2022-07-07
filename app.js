427c1484a78b2431fa024eb90ddd98c4d2277467
shruti0706 <72216138+shruti0706@users.noreply.github.com>
2022-07-07 19:17:18 +0530

    level 3-add environment variables

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); //for encrypting secrets , APIs, etc over internet.
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
@@ -16,8 +17,7 @@ const userSchema=new mongoose.Schema({
   password:String
 });
 
-const secret="Thisisourlittlesecret";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User=new mongoose.model("user",userSchema);
 
